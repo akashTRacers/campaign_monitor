dce96c0194c90f7aafb31935331e79e013494760
akash jain <akjain@grepruby.com>
2018-07-11 19:53:45 +0530

    still working

--- app/controllers/campaign.js
+++ app/controllers/campaign.js
@@ -1,106 +1,48 @@
-const  path='../views/update1.pug';
 const https= require('https');
-var request1 = require('request');
+var requestMaker = require('request');
+const express = require("express");
+const app=express();
 class campaignController{
     
     static getCampaigns(request, response) {
         console.log(">>>>>>>>>>>>>>>>>>>>>>  1 getCampaigns");
-     var options={
-    
-        host:'api.createsend.com',
-        path:'/api/v3.1/clients/e8a260171cc650fd3c7de46ba6f7f0cf/lists.json?pretty=true'
-        ,method: 'GET'
-        ,headers: {
+        const clientId=request.params.clientid;
+        requestMaker('https://api.createsend.com/api/v3.1/clients/'+clientId+'/lists.json?pretty=true', { json: true, method: 'GET',headers: {
             'Authorization': 'Basic ZGNhZWI0MTVlOGM3ZGYyZWU1NmUyMzc5NDJkYzdmZmE=',
             'Content-Type': 'application/json'
+        }}, (err, res, body) => {
+            if (err) { return console.log(err); }
+            console.log(body.message);
+            response.send(body);
+          });
 
-          }
-     };
-     var maybe="";
-       console.log(">>>>>>>>>>>>>>>>>>> 2 getCampaigns");
-       var req= https.get(options,function(res){
-        var body="";
-        res.on('data',function(data){
-            console.log("data has been fetched getCampaigns");
-            body+=data;
-        })
-        res.on('end',function(){
-            maybe=JSON.parse(body);
-            console.log(maybe);
-            response.send(maybe);
-
-        })
-        console.log('here getCampaigns');
-        })
-        req.on('error', function(e) {
-            console.log('Problem with request: ' + e.message);
-        });
+       
     }
 
     static getEmailState(request,response){
         console.log(">>>>>>>>>>>>>>>>>>>>>>  1");
-        var options={
-            host:'api.createsend.com',
-            path:'/api/v3.2/clients/e8a260171cc650fd3c7de46ba6f7f0cf/listsforemail.json?email=jainakash7474@gmail.com'
-            ,method: 'GET'
-            ,headers: {
-                'Authorization': 'Basic ZGNhZWI0MTVlOGM3ZGYyZWU1NmUyMzc5NDJkYzdmZmE=',
-                'Content-Type': 'application/json'
-            }
-        };
-        var maybe="";
-        console.log(">>>>>>>>>>>>>>>>>>> 2");
-        var req= https.get(options,function(res){
-            var body="";
-            res.on('data',function(data){
-                console.log("data has been fetched");
-                body+=data;
-            })
-            res.on('end',function(){
-                maybe=JSON.parse(body);
-                console.log(maybe);
-                response.send(maybe);
-
-            })
-            console.log('here');
-        })
-        req.on('error', function(e) {
-            console.log('Problem with request: ' + e.message);
-        });
+        const clientId=request.params.clientid;
+        const email= request.query.email;
+        console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&',email);
+        requestMaker('https://api.createsend.com/api/v3.2/clients/'+clientId+'/listsforemail.json?email='+email, { json: true, method: 'GET',headers: {
+            'Authorization': 'Basic ZGNhZWI0MTVlOGM3ZGYyZWU1NmUyMzc5NDJkYzdmZmE=',
+            'Content-Type': 'application/json'
+        }}, (err, res, body) => {
+            if (err) { return console.log(err); }
+            console.log(body.message);
+            response.send(body);
+          });
+     
 
     }
      static createCampaign(request,response){
-        console.log(">>>>>>>>>>>>>>>>>>>>>>  1");
-        var options={
-            host:'api.createsend.com',
-            path:'api/v3.2/lists/e8a260171cc650fd3c7de46ba6f7f0cf.json'
-            ,method: 'POST'
-            ,headers: {
-                'Authorization': 'Basic ZGNhZWI0MTVlOGM3ZGYyZWU1NmUyMzc5NDJkYzdmZmE=',
-                'Content-Type': 'application/json'
-            },
-            json:true,
-            body: {
-                "Title": "Website Sky7",
-                "UnsubscribePage": "http://www.example.com/unsubscribed.html",
-                "UnsubscribeSetting": "AllClientLists",
-                "ConfirmedOptIn": false,
-                "ConfirmationSuccessPage": "http://www.example.com/joined.html"
-            }
-        };
-
-        console.log(">>>>>>>>>>>>>>>>>>>>>>createCampaign  2");
-        request1('https://api.createsend.com/api/v3.2/lists/e8a260171cc650fd3c7de46ba6f7f0cf.json', { json: true, method: 'POST',headers: {
+        const clientId=request.params.clientid;
+        console.log("createCampaign  1");
+        console.log(request.body);
+        requestMaker('https://api.createsend.com/api/v3.2/lists/'+clientId+'.json', { json: true, method: 'POST',headers: {
             'Authorization': 'Basic ZGNhZWI0MTVlOGM3ZGYyZWU1NmUyMzc5NDJkYzdmZmE=',
             'Content-Type': 'application/json'},
-            body:{
-                "Title": "Newest Sky9",
-                "UnsubscribePage": "http://www.example.com/unsubscribed.html",
-                "UnsubscribeSetting": "AllClientLists",
-                "ConfirmedOptIn": false,
-                "ConfirmationSuccessPage": "http://www.example.com/joined.html"
-                
-        }}, (err, res, body) => {
+            body:request.body}, (err, res, body) => {
             if (err) { return console.log(err); }
             console.log(body);
             response.send(body);
@@ -111,8 +53,10 @@ class campaignController{
     }
 
     static deleteSegment(request,response){
+        const segmentId=request.params.segmentid;
+       
         console.log(">>>>>>>>>>>>>>>>>>>>>>deleteSegment  1");
-        request1('https://api.createsend.com/api/v3.2/segments/3ee88782fd55e22a22b7d9d90363cbef.json', { json: true, method: 'DELETE',headers: {
+        requestMaker('https://api.createsend.com/api/v3.2/segments/'+segmentId+'.json', { json: true, method: 'DELETE',headers: {
             'Authorization': 'Basic ZGNhZWI0MTVlOGM3ZGYyZWU1NmUyMzc5NDJkYzdmZmE=',
             'Content-Type': 'application/json'
         }}, (err, res, body) => {
@@ -124,8 +68,9 @@ class campaignController{
     }
 
     static getAllSegment(request,response){
-        console.log(">>>>>>>>>>>>>>>>>>>>>>getSegment  1");
-        request1('https://api.createsend.com/api/v3.2/lists/835d66e4ac3a69db3989da7a3ab6f46b/segments.json', { json: true, method: 'GET',headers: {
+        console.log(">>>>>>>>>>>>>>>>>>>>>>geAllSegment  1");
+        const listId = request.params.listid;
+        requestMaker('https://api.createsend.com/api/v3.2/lists/'+listId+'/segments.json', { json: true, method: 'GET',headers: {
             'Authorization': 'Basic ZGNhZWI0MTVlOGM3ZGYyZWU1NmUyMzc5NDJkYzdmZmE=',
             'Content-Type': 'application/json'
         }}, (err, res, body) => {
@@ -138,87 +83,43 @@ class campaignController{
 
     static getSpecificSegment(request,response){
         console.log(">>>>>>>>>>>>>>>>>>>>>>getSegment  1");
-        request1('https://api.createsend.com/api/v3.2/segments/cec5ad087a263c8bde2cc79270071441.json', { json: true, method: 'GET',headers: {
+        const segmentId=request.params.segmentid;
+        requestMaker('https://api.createsend.com/api/v3.2/segments/'+segmentId+'.json', { json: true, method: 'GET',headers: {
             'Authorization': 'Basic ZGNhZWI0MTVlOGM3ZGYyZWU1NmUyMzc5NDJkYzdmZmE=',
             'Content-Type': 'application/json'
         }}, (err, res, body) => {
             if (err) { return console.log(err); }
             console.log(body);
             response.send(body);
-          });
+          }); 
 
     }
 
     static createSegment(request,response){
         console.log(">>>>>>>>>>>>>>>>>>>>>>createSegment  1");
-        request1('https://api.createsend.com/api/v3.2/segments/835d66e4ac3a69db3989da7a3ab6f46b.json', { json: true, method: 'POST',headers: {
+        const listId= request.params.listid;
+        console.log(listId);
+        console.log(request.body);
+        requestMaker('https://api.createsend.com/api/v3.2/segments/'+listId+'.json', { json: true, method: 'POST',headers: {
             'Authorization': 'Basic ZGNhZWI0MTVlOGM3ZGYyZWU1NmUyMzc5NDJkYzdmZmE=',
             'Content-Type': 'application/json'},
-            body:{
-                "Title": "My Rules Segment9",
-                "RuleGroups": [
-                    {
-                        "Rules": [
-                            {
-                                "RuleType": "EmailAddress",
-                                "Clause": "CONTAINS @domain.com"
-                            }
-                        ]
-                    },
-                    {
-                        "Rules": [
-                            {
-                                "RuleType": "DateSubscribed",
-                                "Clause": "AFTER 2009-01-01"
-                            },
-                            {
-                                "RuleType": "DateSubscribed",
-                                "Clause": "EQUALS 2009-01-01"
-                            }
-                        ]
-                    },
-                    {
-                        "Rules": [
-                            {
-                                "RuleType": "DateSubscribed",
-                                "Clause": "BEFORE 2010-01-01"
-                            }
-                        ]
-                    }
-                ]
-            }
+            body:request.body
         }, (err, res, body) => {
             if (err) { return console.log(err); }
-            console.log(body.message);
+            console.log(body);
             response.send(body);
           });
 
     }
    static updateSegment(request,response){
     console.log(">>>>>>>>>>>>>>>>>>>>>>getSegment  1");
-    request1('https://api.createsend.com/api/v3.2/segments/cec5ad087a263c8bde2cc79270071441.json', { json: true, method: 'PUT',headers: {
+    const segmentId=request.params.segmentid;
+    console.log(segmentId);
+    console.log(request.body);
+    requestMaker('https://api.createsend.com/api/v3.2/segments/'+segmentId+'.json', { json: true, method: 'PUT',headers: {
         'Authorization': 'Basic ZGNhZWI0MTVlOGM3ZGYyZWU1NmUyMzc5NDJkYzdmZmE=',
         'Content-Type': 'application/json'
-    },body:{
-        "Title": "My Newest Segment",
-        "RuleGroups":[
-                {
-                  "Rules":[
-                            {
-                            "RuleType":"EmailAddress",
-                            "Clause":"CONTAINS @sky.com"
-                                
-                            },
-                            {
-                            "RuleType": "DateSubscribed",
-                            "Clause": "BEFORE 2018-10-01"
-                            }
-                            
-                        ]
-                }
-            
-            ]
-    }    
+    },body:request.body  
 
     }, (err, res, body) => {
         if (err) { return console.log(err); }
--- app/routes/campaign.js
+++ app/routes/campaign.js
@@ -2,18 +2,16 @@ const express = require("express");
 const campaignController = require("../controllers/campaign");
 const initCampaign = () => {
     const campaignRouter = express.Router();    
-    campaignRouter.get('/', campaignController.getCampaigns);
-     campaignRouter.get('/emaillist', campaignController.getEmailState);
-     campaignRouter.post('/', campaignController.createCampaign);
+    campaignRouter.get('/:clientid', campaignController.getCampaigns);//
+    campaignRouter.get('/emaillist/:clientid', campaignController.getEmailState);//
+    campaignRouter.post('/:clientid', campaignController.createCampaign);//
 
-     //Segment Operation
-     campaignRouter.get('/segments', campaignController.getAllSegment);
-     campaignRouter.post('/segments', campaignController.createSegment);
-     campaignRouter.put('/segments', campaignController.updateSegment);
-     campaignRouter.delete('/segments', campaignController.deleteSegment);
-     campaignRouter.get('/getyoursegment', campaignController.getSpecificSegment);
-     
-     
+    //Segment Operation
+    campaignRouter.get('/segments/:listid', campaignController.getAllSegment);//
+    campaignRouter.post('/segments/:listid', campaignController.createSegment);//
+    campaignRouter.put('/segments/:segmentid', campaignController.updateSegment); //
+    campaignRouter.delete('/segments/:segmentid', campaignController.deleteSegment);//
+    campaignRouter.get('/getyoursegment/:segmentid', campaignController.getSpecificSegment);//
     return campaignRouter;
 }
 module.exports = initCampaign;
